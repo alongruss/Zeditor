b69e29d97922a390199e9a75424283807b2083c9
Adir <42638312+Adir-SL@users.noreply.github.com>
2020-12-14 10:17:15 +0200

    Bug fixes
    Remove button now removes the gizmo and deselect current selection.
    When selecting object with the gizmo, it should now highlight them in the scene tree as well.

--- demos/shapes/shapes.js
+++ demos/shapes/shapes.js
@@ -309,6 +309,7 @@ function removeIlloChildById(element, id) {
       removeIlloChildById(element.children[i], id);
     }
   }
+  deselectAll(illo);
   updateTree();
 }
 
@@ -452,6 +453,7 @@ function deselectAll(element) {
       element.children.splice(i, 1);
     }
   }
+  setTimeout(function(){ testGizmo(); }, 500);
   highLightTree();
 }
 
@@ -468,6 +470,17 @@ function highLightTree() {
       }
     }
   }
+}
+function testGizmo(){
+  var x = illo.children;
+  var i;
+  for (i = 0; i < x.length-1; i++) {
+    if(illo.children[i].renderFront.x == illo.children[illo.children.length-1].renderFront.x && illo.children[i].renderFront.y == illo.children[illo.children.length-1].renderFront.y && illo.children[i].renderFront.z == illo.children[illo.children.length-1].renderFront.z){
+      console.log(illo.children[i].type);
+      illo.children[i].selected = "true";
+      highLightTree();
+    }
+}
 }
 
 function resetControls() {
